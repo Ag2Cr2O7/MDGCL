095680fb33bd38cfc50c579edda236e78963cdc4
Ag2Cr2O8 <131515402+Ag2Cr2O8@users.noreply.github.com>
2023-05-17 00:11:30 +0800

    Update myparser.py

--- myparser.py
+++ myparser.py
@@ -51,7 +51,7 @@ def parse_args(dataset='yelp'):
         parser.add_argument('--Lgraph', default=1e-2, type=float, help='weight of Lgraph')
         parser.add_argument('--t', default=0.4, type=float, help='tao of Ls')
         parser.add_argument('--Lhyper', default=1e-4, type=float, help='weight of Lhyper')
-        parser.add_argument('--temp', default=0.2, type=float, help='tao\' of Lh')
+        parser.add_argument('--temp', default=1, type=float, help='tao\' of Lh')
         parser.add_argument('--earlystop', default=3, type=int, help='earlystop')
         parser.add_argument('--use_log', default=True, type=bool, help='log')
 
